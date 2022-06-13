f00cf616791978855719f928791d4eb13f0d8400
Joey Faris <joeyfaris1@yahoo.com>
2022-06-13 12:17:30 -0700

    commit tree 2ab9e7079fc64213c36a9725b3101308d096695d

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+
+
+function submitData(name, email) {
+    fetch('http://localhost:3000/users,', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'applications/json',
+        'Accept': 'applications/json',
+    },
+
+    body: JSON.stringify( {
+        name,
+        email
+    })
+})
+    .then(response => response.json)
+    }
+
+
+
+submitData('Joey Faris', 'joeyfaris1@yahoo.com');
+
+  
